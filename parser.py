03bace923ee230cef7846191e4051627bf0df305
joshstenis <joshstenis@gmail.com>
2021-05-06 18:06:20 -0500

    Running driver.py did not work

--- parser.py
+++ parser.py
@@ -4,8 +4,6 @@ import sys
 from bison import BisonParser, BisonNode, BisonSyntaxError
 
 bisonFile = 'grammar.y'  # original bison file
-lexFile = 'scanner.yy'    # original flex file
-
 
 class Parser(BisonParser):
 
@@ -17,13 +15,18 @@ class Parser(BisonParser):
     precedences = (
         )
 
+    def read(self, nbytes):
+        try:
+            return input()
+        except EOFError:
+            return ''
+
     start = 'program'
 
     def on_program(self, target, option, names, items):
         """
         program: stmt_list T_SEMICOLON
         """
-        return
 
     def on_stmt_list(self, target, option, names, items):
         """
@@ -166,3 +169,205 @@ class Parser(BisonParser):
         expr_list : a_expr
             | expr_list T_COMMA_DELIM a_expr
         """
+
+    lexscript = r"""
+    %{
+    #include <stdio.h>
+    #include <string.h>
+    #include "Python.h"
+    #define YYSTYPE void *
+    #include "tokens.h"
+    extern void *py_parser;
+    extern void (*py_input)(PyObject *parser, char *buf, int *result, int max_size);
+    #define returntoken(tok) yylval = PyString_FromString(strdup(yytext)); return (tok);
+    #define YY_INPUT(buf,result,max_size) {(*py_input)(py_parser, buf, &result, max_size);}
+
+    #include "simple.h"
+    # undef yywrap
+    # define yywrap() 1
+
+    #undef YY_DECL
+    #define YY_DECL int yylex()
+    YY_DECL;
+
+    // Code run each time a pattern is matched.
+    #undef  YY_USER_ACTION  
+    # define YY_USER_ACTION  {}
+
+    %}
+
+    %option yylineno
+    %option noyywrap 
+
+    DIGIT [0-9] 
+    ALPHA [a-zA-Z]
+
+    %%
+
+    \/\/.*$           { yylloc.last_line++; yylloc.last_column = 0;}	
+    [ ]+						  { yylloc.last_column++;}	
+    [\t]+						  { yylloc.last_column+=2;}	
+    [\n]+						  {yylloc.last_line++; yylloc.last_column = 0;}	
+
+    \".+\"						{ 
+                        yylloc.last_column += strlen(yytext);
+                                            returntoken(T_LITERAL_STR); 
+                    }
+    ";"               {
+                        yyloc.last_column++;
+                        returntoken(T_SEMICOLON);
+                    }
+    ":="							{ 
+                        yylloc.last_column += 2;
+                                            returntoken(T_ASSIGN); 
+                    }
+    ":"               {
+                        yyloc.last_column++;
+                        returntoken(T_COLON);
+                    }
+    "("               {
+                        yyloc.last_column++;
+                        returntoken(T_LPAREN);
+                    }
+    ")"               {
+                        yyloc.last_column++;
+                        returntoken(T_RPAREN);
+                    }
+    "\["               {
+                        yyloc.last_column++;
+                        returntoken(T_LBRACK);
+                    }
+    "\]"               {
+                        yyloc.last_column++;
+                        returntoken(T_RBRACK);
+                    }
+    ", "               {
+                        yyloc.last_column+=2;
+                        returntoken(T_COMMA_DELIM);
+                    }
+    "+"							  { 
+                        yylloc.last_column++;
+                                            returntoken(T_ADD); 
+                    }
+    "-"							  { 
+                        yylloc.last_column++;
+                                            returntoken(T_SUB); 
+                    }
+    "*"							  { 
+                        yylloc.last_column++;
+                                            returntoken(T_MUL); 
+                    }
+    "/"							  { 
+                        yylloc.last_column++;
+                                            returntoken(T_DIV); 
+                    }
+    "<"							  { 
+                        yylloc.last_column++;
+                                            returntoken(T_LT); 
+                    }
+    ">"							  { 
+                        yylloc.last_column++;
+                                            returntoken(T_GT); 
+                    }
+    ">="							{ 
+                        yylloc.last_column+=2;
+                                            returntoken(T_GEQ); 
+                    }
+    "<="							{ 
+                        yylloc.last_column+=2;
+                                            returntoken(T_LEQ); 
+                    }
+    "="							  { 
+                        yylloc.last_column+=1;
+                                            returntoken(T_EQ); 
+                    }
+    "<>"							{ 
+                        yylloc.last_column+=2;
+                                            returntoken(T_NEQ); 
+                    }
+    "and"							{ 
+                        yylloc.last_column+=3;
+                                            returntoken(T_AND); 
+                    }
+    "or"							{ 
+                        yylloc.last_column+=2;
+                                            returntoken(T_OR); 
+                    }
+    "foreach"				  { 
+                        yylloc.last_column+=7;
+                                            returntoken(T_FOREACH); 
+                    }
+    "in"				      { 
+                        yylloc.last_column+=7;
+                                            returntoken(T_IN); 
+                    }
+    "while"				    { 
+                        yylloc.last_column+=5;
+                                            returntoken(T_WHILE); 
+                    }
+    "begin"					  { 
+                        yylloc.last_column+=5;
+                                            returntoken(T_BEGIN); 
+                    }
+    "end"				      { 
+                        yylloc.last_column+=3;
+                                            returntoken(T_END); 
+                    }
+    "if"				      { 
+                        yylloc.last_column+=2;
+                                            returntoken(T_IF); 
+                    }
+    "then"				    { 
+                        yylloc.last_column+=4;
+                                            returntoken(T_THEN); 
+                    }
+    "else"				    { 
+                        yylloc.last_column+=4;
+                                            returntoken(T_ELSE); 
+                    }
+    "write"						{ 
+                        yylloc.last_column+=5;
+                                            returntoken(T_WRITE); 
+                    }
+    "read"						{ 
+                        yylloc.last_column+=4;
+                                            returntoken(T_READ); 
+                    }
+    "int"							{ 
+                        yylloc.last_column+=3;
+                                            returntoken(T_INTEGER); 
+                    }
+    "float"						{ 
+                        yylloc.last_column+=5;
+                                            returntoken(T_FLOAT); 
+                    }
+    "repeat"				  { 
+                        yylloc.last_column+=6;
+                                            returntoken(T_REPEAT); 
+                    }
+    "until"				    { 
+                        yylloc.last_column+=5;
+                                            returntoken(T_UNTIL); 
+                    }
+    "declare"					{ 
+                        yylloc.last_column+=7;
+                                            returntoken(T_DECLARE); 
+                    }
+    {DIGIT}+[.]{DIGIT}+	{ 
+                        yylloc.last_column += strlen(yytext);
+                        returntoken(T_NUM);
+                                        }
+    {DIGIT}+					{ 
+                        yylloc.last_column += strlen(yytext);
+                                            returntoken(T_NUM);
+                                        }
+    ({ALPHA}|[_])({DIGIT}|{ALPHA}|[_])*     { 
+                                            yylloc.last_column += strlen(yytext);
+                                                                                        returntoken(T_ID);
+                                                                                    }
+    .									{  yylloc.last_column++; returntoken(yytext[0]);}
+
+    %%
+
+    yywrap() { return(1);}
+    """
