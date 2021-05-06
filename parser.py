bef2616045b02323678a422cd9cc50eddc67a191
joshstenis <joshstenis@gmail.com>
2021-05-06 16:53:27 -0500

    Cleared parser.py for conversion

--- parser.py
+++ parser.py
@@ -1,18 +1,51 @@
-import pybison
+#!/usr/bin/env python
 
-class Parser(object, stream):
-    def __init__(self):
-        self.stream = stream
+"""
+PyBison file automatically generated from grammar file grammar.y
+You can edit this module, or import it and subclass the Parser class
+"""
 
-    def parse(self):
-        # grammar rules defined here
+import sys
 
-        # output PASS or FAIL based on grammar
+from bison import BisonParser, BisonNode, BisonSyntaxError
 
-def main():
-    # create parser object
+bisonFile = 'grammar.y'  # original bison file
+lexFile = 'scanner.yy'    # original flex file
 
-    # loop input seekage
 
-if __name__ == "__main__":
-    main()
+class Parser(BisonParser):
+    """
+    bison Parser class generated automatically by bison2py from the
+    grammar file "grammar.y" and lex file "scanner.yy"
+
+    You may (and probably should) edit the methods in this class.
+    You can freely edit the rules (in the method docstrings), the
+    tokens list, the start symbol, and the precedences.
+
+    Each time this class is instantiated, a hashing technique in the
+    base class detects if you have altered any of the rules. If any
+    changes are detected, a new dynamic lib for the parser engine
+    will be generated automatically.
+    """
+
+    # -------------------------------------------------
+    # Default class to use for creating new parse nodes
+    # -------------------------------------------------
+    defaultNodeClass = BisonNode
+
+    # --------------------------------------------
+    # basename of binary parser engine dynamic lib
+    # --------------------------------------------
+    bisonEngineLibName = 'parser-engine'
+
+    # ----------------------------------------------------------------
+    # lexer tokens - these must match those in your lex script (below)
+    # ----------------------------------------------------------------
+    tokens = ['T_ID', 'T_NUM', 'T_ADD', 'T_SUB', 'T_MUL', 'T_DIV', 'T_LT', 'T_GT', 'T_LEQ', 'T_GEQ', 'T_EQ', 'T_NEQ', 'T_AND', 'T_OR', 'T_READ', 'T_WRITE', 'T_ASSIGN', 'T_BEGIN', 'T_END', 'T_FOREACH', 'T_IN', 'T_REPEAT', 'T_UNTIL', 'T_WHILE', 'T_IF', 'T_THEN', 'T_ELSE', 'T_DECLARE', 'T_INTEGER', 'T_FLOAT', 'T_LITERAL_STR']
+
+    # ------------------------------
+    # precedences
+    # ------------------------------
+    precedences = (
+        )
+
