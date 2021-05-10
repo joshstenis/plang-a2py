f7250f24c63fbd3ce8490069a3a19eba38760be4
joshstenis <joshstenis@gmail.com>
2021-05-09 21:22:49 -0500

    IT RUNS!!!!! Nothing happens i.e. no output, BUT IT RUNSSS

--- parser.py
+++ parser.py
@@ -277,12 +277,35 @@ class Parser(BisonParser):
     will be generated automatically.
     """
 
-    lexscript = lexFile
-    bisonFile = bisonFile
+    bisonCmd = ['bison', '-d', '-v', '-t']
+    bisonFile = 'tmp.y'
+    bisonCFile = 'tmp.tab.c'
+    bisonHFIle = 'tmp.tab.h'
+
+    flexCmd = ['flex', '-o']
+    flexFile = 'tmp.yy'
+    flexCFile = 'lex.yy.c'
+
+    cflags_pre = ['-fPIC']
+    cflags_post = ['-O3', '-g']
+
+    file = None
+    keepfiles = 0
+
     bisonEngineLibName = 'parser2-engine'
 
     tokens = ['T_ID', 'T_NUM', 'T_ADD', 'T_SUB', 'T_MUL', 'T_DIV', 'T_LT', 'T_GT', 'T_LEQ', 'T_GEQ', 'T_EQ', 'T_NEQ', 'T_AND', 'T_OR', 'T_READ', 'T_WRITE', 'T_ASSIGN', 'T_BEGIN', 'T_END', 'T_FOREACH', 'T_IN', 'T_REPEAT', 'T_UNTIL', 'T_WHILE', 'T_IF', 'T_THEN', 'T_ELSE', 'T_DECLARE', 'T_INTEGER', 'T_FLOAT', 'T_LITERAL_STR', 'T_SEMICOLON', 'T_COLON', 'T_LPAREN', 'T_RPAREN', 'T_LBRACK', 'T_RBRACK', 'T_COMMA_DELIM']
     precedences = ()
+
+    # -------------------------------------
+    # Override default read method
+    # -------------------------------------
+    def read(self, nbytes):
+        try:
+            return input() + '\n'
+        except:
+            return ''
+
     start = 'program'
 
     def on_program(self, target, option, names, items):
@@ -520,11 +543,8 @@ class Parser(BisonParser):
                             names=names, 
                             items=items)
 
+    lexscript = lexFile
+
 if __name__ == "__main__":
     p = Parser()
-    prgm = input()
-    try:
-        p.run(file=prgm)
-        print("PASS")
-    except:
-        print("FAIL")
+    p.run()
