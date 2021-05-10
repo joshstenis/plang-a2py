987566d5691f3e4e73a555675a7b6d892108faba
joshstenis <joshstenis@gmail.com>
2021-05-10 15:44:38 -0500

    Removed unneeded whitespace from lexscript field

--- parser.py
+++ parser.py
@@ -299,11 +299,14 @@ class Parser(BisonParser):
     def read(self, nbytes):
         try:
             return input() + '\n'
+        except BisonSyntaxError:
+            print('Invalid token\n')
+            sys.exit()
         except EOFError:
             return ''
 
-    def error(self, node):
-        return 'Invalid token: %s', node.target
+    # def error(self, node):
+    #     return 'Invalid token: %s', node.target
 
     start = 'program'
 
@@ -311,24 +314,20 @@ class Parser(BisonParser):
         """
         program : stmt_list T_SEMICOLON
         """
-        node = program_Node(target=target, 
+        return program_Node(target=target, 
                             option=option, 
                             names=names, 
                             items=items)
-        # if option != 0:
-        #     return self.error(node)
-        # else:
-        return node
 
     def on_stmt_list(self, target, option, names, items):
         """
         stmt_list : stmt_list T_SEMICOLON stmt
             | stmt
         """
-        if option == 0:
-            print("Tokens: %s : %s", names[0], names[2])
-        else:
-            print("Tokens: %s", names[0])
+        # if option == 0:
+        #     print("Tokens: %s : %s", names[0], names[2])
+        # else:
+        #     print("Tokens: %s", names[0])
         
         return stmt_list_Node(target=target, 
                             option=option, 
@@ -563,19 +562,16 @@ extern void (*py_input)(PyObject *parser, char *buf, int *result, int max_size);
 #define YY_INPUT(buf,result,max_size) {(*py_input)(py_parser, buf, &result, max_size);}
 
 #include "simple.h"
-//# undef yywrap
-//# define yywrap() 1
+# undef yywrap
+# define yywrap() 1
 
-
-
-//#undef YY_DECL
-//#define YY_DECL int yylex()
-//YY_DECL;
-
+#undef YY_DECL
+#define YY_DECL int yylex()
+YY_DECL;
 
 // Code run each time a pattern is matched.
-//#undef  YY_USER_ACTION  
-//# define YY_USER_ACTION  {}
+#undef  YY_USER_ACTION  
+# define YY_USER_ACTION  {}
 
 %}
 
@@ -591,12 +587,10 @@ ALPHA [a-zA-Z]
 [ ]+						  { yylloc.last_column++;}	
 [\t]+						  { yylloc.last_column+=2;}	
 [\n]+						  {yylloc.last_line++; yylloc.last_column = 0;}	
-
 \".+\"						{ 
                     yylloc.last_column += strlen(yytext);
                                         returntoken(T_LITERAL_STR); 
                 }
-
 ";"               {
                     yylloc.last_column++;
                     returntoken(T_SEMICOLON);
@@ -633,155 +627,124 @@ ALPHA [a-zA-Z]
                     yylloc.last_column++;
                                         returntoken(T_ADD); 
                 }
-
 "-"							  { 
                     yylloc.last_column++;
                                         returntoken(T_SUB); 
                 }
-
 "\*"							  { 
                     yylloc.last_column++;
                                         returntoken(T_MUL); 
                 }
-
 "/"							  { 
                     yylloc.last_column++;
                                         returntoken(T_DIV); 
                 }
-
 "<"							  { 
                     yylloc.last_column++;
                                         returntoken(T_LT); 
                 }
-
 ">"							  { 
                     yylloc.last_column++;
                                         returntoken(T_GT); 
                 }
-
 ">="							{ 
                     yylloc.last_column+=2;
                                         returntoken(T_GEQ); 
                 }
-
 "<="							{ 
                     yylloc.last_column+=2;
                                         returntoken(T_LEQ); 
                 }
-
 "="							  { 
                     yylloc.last_column+=1;
                                         returntoken(T_EQ); 
                 }
-
 "<>"							{ 
                     yylloc.last_column+=2;
                                         returntoken(T_NEQ); 
                 }
-
 "and"							{ 
                     yylloc.last_column+=3;
                                         returntoken(T_AND); 
                 }
-
 "or"							{ 
                     yylloc.last_column+=2;
                                         returntoken(T_OR); 
                 }
-
 "foreach"				  { 
                     yylloc.last_column+=7;
                                         returntoken(T_FOREACH); 
                 }
-
 "in"				      { 
                     yylloc.last_column+=2;
                                         returntoken(T_IN); 
                 }
-
 "while"				    { 
                     yylloc.last_column+=5;
                                         returntoken(T_WHILE); 
                 }
-
 "begin"					  { 
                     yylloc.last_column+=5;
                                         returntoken(T_BEGIN); 
                 }
-
 "end"				      { 
                     yylloc.last_column+=3;
                                         returntoken(T_END); 
                 }
-
 "if"				      { 
                     yylloc.last_column+=2;
                                         returntoken(T_IF); 
                 }
-
 "then"				    { 
                     yylloc.last_column+=4;
                                         returntoken(T_THEN); 
                 }
-
 "else"				    { 
                     yylloc.last_column+=4;
                                         returntoken(T_ELSE); 
                 }
-
 "write"						{ 
                     yylloc.last_column+=5;
                                         returntoken(T_WRITE); 
                 }
-
 "read"						{ 
                     yylloc.last_column+=4;
                                         returntoken(T_READ); 
                 }
-
 "int"							{ 
                     yylloc.last_column+=3;
                                         returntoken(T_INTEGER); 
                 }
-
 "float"						{ 
                     yylloc.last_column+=5;
                                         returntoken(T_FLOAT); 
                 }
-
 "repeat"				  { 
                     yylloc.last_column+=6;
                                         returntoken(T_REPEAT); 
                 }
-
 "until"				    { 
                     yylloc.last_column+=5;
                                         returntoken(T_UNTIL); 
                 }
-
 "declare"					{ 
                     yylloc.last_column+=7;
                                         returntoken(T_DECLARE); 
                 }
-
-
 {DIGIT}+[.]{DIGIT}+	{ 
                     yylloc.last_column += strlen(yytext);
                     returntoken(T_NUM);
                                     }
-
 {DIGIT}+					{ 
                     yylloc.last_column += strlen(yytext);
                                         returntoken(T_NUM);
                                     }
-
 ({ALPHA}|[_])({DIGIT}|{ALPHA}|[_])*     { 
                                         yylloc.last_column += strlen(yytext);
                                                                                     returntoken(T_ID);
                                                                                 }
 
 .									{  yylloc.last_column++; returntoken(yytext[0]);}
-
 
 %%
 
