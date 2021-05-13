1afd77f2182a05da2a06d0d2f97ddb2d01cbcfc0
joshstenis <joshstenis@gmail.com>
2021-05-12 21:19:24 -0500

    Moved docstring ''' ''' indicators for simplicity

--- ply-parser.py
+++ ply-parser.py
@@ -52,8 +52,8 @@ t_WRITE = r'write'
 t_READ = r'read'
 t_REPEAT = r'repeat'
 t_UNTIL = r'until'
-t_NUM = r'[0-9]+[.][0-9]+ | [0-9]+'
-t_ID = r'([a-zA-Z]|[_])([0-9]|[a-zA-Z]|[_])*'
+t_NUM = r'\d+\.\d+|\d+'
+t_ID = r'([a-zA-Z_])\w*'
 
 def t_error(t):
     # print('Invalid character: {}'.format(t.value[0]))
@@ -69,146 +69,102 @@ lexer = lex.lex()
 # ------------------------------------------
 
 def p_program(t):
-    '''
-    program : stmt_list SEMICOLON
-    '''
+    '''program : stmt_list SEMICOLON'''
 
 def p_stmt_list(t):
-    '''
-    stmt_list : stmt_list SEMICOLON stmt
-        | stmt
-    '''
+    '''stmt_list : stmt_list SEMICOLON stmt 
+        | stmt'''
 
 def p_stmt(t):
-    '''
-    stmt : assignment
+    '''stmt : assignment 
         | read 
-        | write
-        | while
-        | repeat
-        | block
-        | foreach
-        | if_stmt
-    '''
+        | write 
+        | while 
+        | repeat 
+        | block 
+        | foreach 
+        | if_stmt'''
 
 def p_block(t):
-    '''
-    block : BEGIN stmt_list END
-    '''
+    '''block : BEGIN stmt_list END'''
 
 def p_foreach(t):
-    '''
-    foreach : FOREACH ID IN LPAREN a_fact COLON a_fact RPAREN stmt
-    '''
+    '''foreach : FOREACH ID IN LPAREN a_fact COLON a_fact RPAREN stmt'''
 
 def p_while(t):
-    '''
-    while : WHILE l_expr block
-    '''
+    '''while : WHILE l_expr block'''
 
 def p_repeat(t):
-    '''
-    repeat : REPEAT stmt_list UNTIL l_expr
-    '''
+    '''repeat : REPEAT stmt_list UNTIL l_expr'''
 
 def p_if_stmt(t):
-    '''
-    if_stmt : IF l_expr THEN stmt_list ELSE else_stmt
-    '''
+    '''if_stmt : IF l_expr THEN stmt_list ELSE else_stmt'''
 
 def p_else_stmt(t):
-    '''
-    else_stmt : 
-        | ELSE stmt
-    '''
+    '''else_stmt : 
+        | ELSE stmt'''
 
 def p_assignment(t):
-    '''
-    assignment : varref ASSIGN l_expr
-    '''
+    '''assignment : varref ASSIGN l_expr'''
 
 def p_a_expr(t):
-    '''
-    a_expr : a_expr ADD a_term
+    '''a_expr : a_expr ADD a_term
         | a_expr SUB a_term
-        | a_term
-    '''
+        | a_term'''
 
 def p_a_term(t):
-    '''
-    a_term : a_term MUL a_fact
+    '''a_term : a_term MUL a_fact
         | a_term DIV a_fact
-        | a_fact
-    '''
+        | a_fact'''
 
 def p_a_fact(t):
-    '''
-    a_fact : varref
-        | NUM
-        | LITERAL_STR
-        | SUB a_fact
-        | LPAREN a_expr RPAREN
-    '''
+    '''a_fact : varref 
+        | NUM 
+        | LITERAL_STR 
+        | SUB a_fact 
+        | LPAREN a_expr RPAREN'''
 
 def p_varref(t):
-    '''
-    varref : ID
-        | varref LBRACK a_expr RBRACK
-    '''
+    '''varref : ID 
+        | varref LBRACK a_expr RBRACK'''
 
 def p_l_expr(t):
-    '''
-    l_expr : l_expr AND l_term
-        | l_term
-    '''
+    '''l_expr : l_expr AND l_term
+        | l_term'''
 
 def p_l_term(t):
-    '''
-    l_term : l_term OR l_fact
-        | l_fact
-    '''
+    '''l_term : l_term OR l_fact
+        | l_fact'''
 
 def p_l_fact(t):
-    '''
-    l_fact : l_fact oprel a_expr
+    '''l_fact : l_fact oprel a_expr
         | a_expr
-        | LPAREN l_expr RPAREN
-    '''
+        | LPAREN l_expr RPAREN'''
 
 def p_oprel(t):
-    '''
-    oprel : LT
+    '''oprel : LT
         | GT
         | LEQ
         | GEQ
         | EQ
-        | NEQ
-    '''
+        | NEQ'''
 
 def p_read(t):
-    '''
-    read : READ varlist
-    '''
+    '''read : READ varlist'''
 
 def p_write(t):
-    '''
-    write : WRITE expr_list
-    '''
+    '''write : WRITE expr_list'''
 
 def p_varlist(t):
-    '''
-    varlist : varref
-        | varref COMMA_DELIM varlist
-    '''
+    '''varlist : varref
+        | varref COMMA_DELIM varlist'''
 
 def p_expr_list(t):
-    '''
-    expr_list : a_expr
-        | expr_list COMMA_DELIM a_expr
-    '''
+    '''expr_list : a_expr
+        | expr_list COMMA_DELIM a_expr'''
 
 def p_error(t):
-    print('Syntax error at {}\n'.format(t))               # at {}'.format(t.value)
+    print('Syntax error: {}\n'.format(t))               # at {}'.format(t.value)
 
 import ply.yacc as yacc
 parser = yacc.yacc()
