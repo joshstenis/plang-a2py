d7ce0391cc6b061c7ecaaad0eb9ca2a722733d2b
joshstenis <joshstenis@gmail.com>
2021-05-12 23:38:21 -0500

    Small parameter change on parser targets

--- ply-parser.py
+++ ply-parser.py
@@ -71,14 +71,14 @@ lexer = lex.lex()
 # Grammar rules defined below
 # ------------------------------------------
 
-def p_program(t):
+def p_program(p):
     '''program : stmt_list SEMICOLON'''
 
-def p_stmt_list(t):
+def p_stmt_list(p):
     '''stmt_list : stmt_list SEMICOLON stmt 
         | stmt'''
 
-def p_stmt(t):
+def p_stmt(p):
     '''stmt : assignment 
         | read 
         | write 
@@ -88,63 +88,63 @@ def p_stmt(t):
         | foreach 
         | if_stmt'''
 
-def p_block(t):
+def p_block(p):
     '''block : BEGIN stmt_list END'''
 
-def p_foreach(t):
+def p_foreach(p):
     '''foreach : FOREACH ID IN LPAREN a_fact COLON a_fact RPAREN stmt'''
 
-def p_while(t):
+def p_while(p):
     '''while : WHILE l_expr block'''
 
-def p_repeat(t):
+def p_repeat(p):
     '''repeat : REPEAT stmt_list UNTIL l_expr'''
 
-def p_if_stmt(t):
+def p_if_stmt(p):
     '''if_stmt : IF l_expr THEN stmt_list ELSE else_stmt'''
 
-def p_else_stmt(t):
+def p_else_stmt(p):
     '''else_stmt : 
         | ELSE stmt'''
 
-def p_assignment(t):
+def p_assignment(p):
     '''assignment : varref ASSIGN l_expr'''
 
-def p_a_expr(t):
+def p_a_expr(p):
     '''a_expr : a_expr ADD a_term
         | a_expr SUB a_term
         | a_term'''
 
-def p_a_term(t):
+def p_a_term(p):
     '''a_term : a_term MUL a_fact
         | a_term DIV a_fact
         | a_fact'''
 
-def p_a_fact(t):
+def p_a_fact(p):
     '''a_fact : varref 
         | NUM 
         | LITERAL_STR 
         | SUB a_fact 
         | LPAREN a_expr RPAREN'''
 
-def p_varref(t):
+def p_varref(p):
     '''varref : ID 
         | varref LBRACK a_expr RBRACK'''
 
-def p_l_expr(t):
+def p_l_expr(p):
     '''l_expr : l_expr AND l_term
         | l_term'''
 
-def p_l_term(t):
+def p_l_term(p):
     '''l_term : l_term OR l_fact
         | l_fact'''
 
-def p_l_fact(t):
+def p_l_fact(p):
     '''l_fact : l_fact oprel a_expr
         | a_expr
         | LPAREN l_expr RPAREN'''
 
-def p_oprel(t):
+def p_oprel(p):
     '''oprel : LT
         | GT
         | LEQ
@@ -152,22 +152,22 @@ def p_oprel(t):
         | EQ
         | NEQ'''
 
-def p_read(t):
+def p_read(p):
     '''read : READ varlist'''
 
-def p_write(t):
+def p_write(p):
     '''write : WRITE expr_list'''
 
-def p_varlist(t):
+def p_varlist(p):
     '''varlist : varref
         | varref COMMA_DELIM varlist'''
 
-def p_expr_list(t):
+def p_expr_list(p):
     '''expr_list : a_expr
         | expr_list COMMA_DELIM a_expr'''
 
-def p_error(t):
-    print('Parsing error: ({0}, \'{1}\') at line {2}'.format(t.type, t.value, t.lexer.lineno))
+def p_error(p):
+    print('Parsing error: ({0}, \'{1}\') at line {2}'.format(p.type, p.value, p.lexer.lineno))
 
 import ply.yacc as yacc
 parser = yacc.yacc()
