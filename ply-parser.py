efe68c534f2e909bb9406615ab0648cd7e9bac7f
joshstenis <joshstenis@gmail.com>
2021-05-12 20:20:18 -0500

    Code has trouble with ID and UNTIL tokens

--- ply-parser.py
+++ ply-parser.py
@@ -21,47 +21,45 @@ tokens = (
 
 t_ignore = ' \t\n'
 t_LITERAL_STR = r'\".+\"'
-t_SEMICOLON = r';'
-t_ASSIGN = r':='
-t_COLON = r':'
+t_SEMICOLON = ';'
+t_ASSIGN = ':='
+t_COLON = ':'
 t_LPAREN = r'\('
 t_RPAREN =  r'\)'
 t_LBRACK = r'\['
-t_RBRACK = r'\]'
-t_COMMA_DELIM = r', '
+t_RBRACK = ']'
+t_COMMA_DELIM = ', '
 t_ADD = r'\+'
-t_SUB = r'-'
+t_SUB = '-'
 t_MUL = r'\*'
-t_DIV = r'/'
-t_LT = r'<'
-t_GT = r'>'
-t_LEQ = r'<='
-t_GEQ = r'>='
-t_EQ = r'=='
-t_NEQ = r'!='
+t_DIV = '/'
+t_LT = '<'
+t_GT = '>'
+t_LEQ = '<='
+t_GEQ = '>='
+t_EQ = '=='
+t_NEQ = '!='
 t_AND = 'and'
-t_OR = r'or'
-t_FOREACH = r'foreach'
-t_IN = r'in'
-t_WHILE = r'while'
-t_BEGIN = r'begin'
-t_END = r'end'
-t_IF = r'if'
-t_THEN = r'then'
-t_ELSE = r'else'
-t_WRITE = r'write'
-t_READ = r'read'
-t_INTEGER = r'int'
-t_FLOAT = r'float'
-t_REPEAT = r'repeat'
-t_UNTIL = r'until'
-t_DECLARE = r'declare'
+t_OR = 'or'
+t_FOREACH = 'foreach'
+t_IN = 'in'
+t_WHILE = 'while'
+t_BEGIN = 'begin'
+t_END = 'end'
+t_IF = 'if'
+t_THEN = 'then'
+t_ELSE = 'else'
+t_WRITE = 'write'
+t_READ = 'read'
+t_REPEAT = 'repeat'
+t_UNTIL = 'until'
 t_NUM = r'[0-9]+[.][0-9]+ | [0-9]+'
 t_ID = r'([a-zA-Z] | [_])([0-9]] | [a-zA-Z] | [_])*'
 
 def t_error(t):
-    print('Invalid character: {}'.format(t.value[0]))
-    t.lexer.skip(1)
+    # print('Invalid character: {}'.format(t.value[0]))
+    # t.lexer.skip(1)
+    pass
 
 import ply.lex as lex
 lexer = lex.lex()
@@ -72,7 +70,9 @@ lexer = lex.lex()
 # ------------------------------------------
 
 def p_program(t):
-    '''program : stmt_list SEMICOLON'''
+    '''
+    program : stmt_list SEMICOLON
+    '''
 
 def p_stmt_list(t):
     '''
@@ -93,19 +93,29 @@ def p_stmt(t):
     '''
 
 def p_block(t):
-    '''block : BEGIN stmt_list END'''
+    '''
+    block : BEGIN stmt_list END
+    '''
 
 def p_foreach(t):
-    '''foreach : FOREACH ID IN LPAREN a_fact COLON a_fact RPAREN  stmt'''
+    '''
+    foreach : FOREACH ID IN LPAREN a_fact COLON a_fact RPAREN stmt
+    '''
 
 def p_while(t):
-    '''while : WHILE l_expr block'''
+    '''
+    while : WHILE l_expr block
+    '''
 
 def p_repeat(t):
-    '''repeat : REPEAT stmt_list UNTIL l_expr'''
+    '''
+    repeat : REPEAT stmt_list UNTIL l_expr
+    '''
 
 def p_if_stmt(t):
-    '''if_stmt : IF l_expr THEN stmt_list ELSE else_stmt'''
+    '''
+    if_stmt : IF l_expr THEN stmt_list ELSE else_stmt
+    '''
 
 def p_else_stmt(t):
     '''
@@ -114,7 +124,9 @@ def p_else_stmt(t):
     '''
 
 def p_assignment(t):
-    '''assignment : varref ASSIGN l_expr'''
+    '''
+    assignment : varref ASSIGN l_expr
+    '''
 
 def p_a_expr(t):
     '''
@@ -175,10 +187,14 @@ def p_oprel(t):
     '''
 
 def p_read(t):
-    '''read : READ varlist'''
+    '''
+    read : READ varlist
+    '''
 
 def p_write(t):
-    '''write : COMMA_DELIM expr_list'''
+    '''
+    write : WRITE expr_list
+    '''
 
 def p_varlist(t):
     '''
@@ -193,7 +209,7 @@ def p_expr_list(t):
     '''
 
 def p_error(t):
-    print('Syntax error\n')               # at {}'.format(t.value)
+    print('Syntax error at {}\n'.format(t))               # at {}'.format(t.value)
 
 import ply.yacc as yacc
 parser = yacc.yacc()
@@ -203,11 +219,5 @@ parser = yacc.yacc()
 # Code executed here
 # ------------------------------------------
 
-# while True:
-#     try:
-#         prgm = input()
-#     except EOFError:
-#         break
-
-prgm = open('inputs/expr1_pass.smp', 'r').read()
+prgm = open('inputs/{}.smp'.format(input('File: ')), 'r').read()
 parser.parse(prgm)
